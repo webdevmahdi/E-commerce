c35ab344e6df53644d4de242010b55887abb57fb
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-08 23:36:49 +0600

    short problems solved

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
         <NavLink to='/home'>Home</NavLink>
-        <NavLink to='/  orderReview'>Order review</NavLink>
+        <NavLink to='/orderReview'>Order review</NavLink>
     </div>
   )
 }
 
-export default Header
+export default Header;
