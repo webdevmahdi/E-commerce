6635d2c7fd34026ad66cd62d1851a00f47d28d5f
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-09 09:32:42 +0600

    header style added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className='menu'>
         <NavLink to='/home'>Home</NavLink>
         <NavLink to='/orderReview'>Order review</NavLink>
     </div>
