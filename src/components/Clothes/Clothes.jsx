81af8526a3bdc7a59b7dc1a760634370171d90e6
webdevmahdi <mahdihasan01705259@gmail.com>
2023-09-09 10:08:29 +0600

    all page styled

--- src/components/Clothes/Clothes.jsx
+++ src/components/Clothes/Clothes.jsx
@@ -4,10 +4,10 @@ import './Clothes.css'
 const Clothes = (props) => {
     let {name, picture, price} = props.clothe;
   return (
-    <div className='clothes-container'>
+    <div className='clothe-container'>
         <img src={picture}></img>
-        <h3>Name: {name}</h3>
         <p>Price: ${price}</p>
+        <h3>Name: {name}</h3>
         <button>Add to Cart</button>
     </div>
   )
